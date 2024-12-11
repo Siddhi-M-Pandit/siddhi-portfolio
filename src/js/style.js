9e8c2b12067b3e7c21a0d457bafe003a640567a6
Siddhi M Pandit <smpandit@csuchico.edu>
2024-12-11 15:18:02 -0800

    last commit

--- src/js/style.js
+++ src/js/style.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const filterButtons = document.querySelectorAll('.filter-btn');
     const projectCards = document.querySelectorAll('.project-card');
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 
     filterButtons.forEach(button => {
         button.addEventListener('click', () => {
@@ -17,5 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
             filterButtons.forEach(btn => btn.classList.remove('active'));
             button.classList.add('active');
         });
+    });
+
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl);
     });
 });
